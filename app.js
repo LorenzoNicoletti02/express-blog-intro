c2711c035f1e55c67e95c8bf0a955d6a15b1bba2
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-12-02 19:54:48 +0100

    feat: fine esercizio mas con errore 404 in postman

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Fede");
+const express = require("express");
+const app = express();
+
+// Importato il file Data.js che ci esporta un oggetto con al suo interno l'array delle ricette
+const {arrayRicette} = require("./data");
+
+
+app.use(express.static("public"));
+
+// Listen Serve per accendere il server e metterlo in ascolto per le chiamate API
+app.listen(3000, () => {
+})
+
+// Creo una rotta 
+app.get("/", (request, response) => {
+    console.log("Server del mio blog");
+    response.send("<h1>Ciaone vengo dal server</h1>");
+})
+
+app.get("/Bacheca", (request, response) => {
+    const objectReturn = {arrayRicette};
+    response.json(objectReturn);
+})
+
+
